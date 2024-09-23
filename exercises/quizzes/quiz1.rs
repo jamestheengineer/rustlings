a82c81a899cf0265633ffe3e7d4d825e24277445
jamestheengineer <jamestheengineer@gmail.com>
2024-09-23 19:13:58 -0400

    take a quiz

--- exercises/quizzes/quiz1.rs
+++ exercises/quizzes/quiz1.rs
@@ -13,7 +13,16 @@
 // fn calculate_price_of_apples(???) -> ??? { ??? }
 
 fn main() {
-    // You can optionally experiment here.
+    println!("{}",calculate_price_of_apples(4));
+    println!("{}",calculate_price_of_apples(50));
+}
+
+fn calculate_price_of_apples(num:i32) -> i32 {
+    if num > 40 {
+        num
+    } else {
+        num *2
+    }
 }
 
 // Don't change the tests!
