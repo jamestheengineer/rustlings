63deb0196b2fcee06e1482b3c3835d6434c7f346
jamestheengineer <jamestheengineer@gmail.com>
2024-10-21 19:19:58 -0400

    partial solution

--- exercises/quizzes/quiz2.rs
+++ exercises/quizzes/quiz2.rs
@@ -27,7 +27,9 @@ mod my_module {
     use super::Command;
 
     // TODO: Complete the function as described above.
-    // pub fn transformer(input: ???) -> ??? { ??? }
+    pub fn transformer(input: Vec<(String, Command)>) -> Vec<String> { 
+         vec!["test".to_string(),"test".to_string()]
+    }
 }
 
 fn main() {
@@ -39,6 +41,7 @@ mod tests {
     // TODO: What do we need to import to have `transformer` in scope?
     // use ???;
     use super::Command;
+    use crate::my_module::transformer;
 
     #[test]
     fn it_works() {
